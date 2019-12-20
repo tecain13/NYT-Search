2303bc0309423689b5d4a8fb59d7164d10875d74
Tyler Cain <tecain13@gmail.com>
2019-12-19 20:50:59 -0700

    proper string syntax implemented

--- NYTCode.js
+++ NYTCode.js
@@ -8,24 +8,42 @@ function usersearch() {
     }).then(function (res) {
         //         (this).attr
         // search - term.val.trim()
-        console.log(res)
-            ;
+        console.log(res);
     }
-
     )
 };
-
 usersearch()
 
-$("#run-search").on("click", function (event) {
-    event.preventDefault();
+function buildQueryURL() {
     var searchterm = $("#search-term").val().trim();
-    var startyear = $("#start-year").val().trim();
-    var endyear = $("#end-year").val().trim();
+    var startyear = $("#start-year").val().trim() + "0101";
+    var endyear = $("#end-year").val().trim() + "1231";
+    // var startyear = '19900101'
+    // var endyear = '19990101'
 
     console.log(searchterm);
     console.log(startyear);
     console.log(endyear);
+
+    var APIkey = "ISIuRoxErOd0pVSE6l9jL9V52oWkuhFP";
+    var nytimes = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+    var URL = nytimes + "q=" + searchterm + "&end_date=" + endyear + "&begin_date=" + startyear + "&api-key=" + APIkey
+    console.log(URL);
+
+    return URL;
+
+};
+
+function clear() {
+    $("#article-section").empty();
+};
+
+
+
+$("#run-search").on("click", function (event) {
+    event.preventDefault();
+    clear();
+    buildQueryURL()
 });
 
 
